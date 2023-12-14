077b39c2d494bf6f5db91a32f4a2b1cd7775e4e6
cgonzalezh23 <claudiogonzalez.hernandez@brightspeed.com>
2023-12-14 17:28:51 -0600

    stack update

--- src/pages/TecnologiesStack/TecnologiesStack.jsx
+++ src/pages/TecnologiesStack/TecnologiesStack.jsx
@@ -1,61 +1,185 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { Container, Row, Card, Col } from "react-bootstrap";
+import { Container, Row, Card, Col, ListGroup } from "react-bootstrap";
+import contentStack from "../../utils/contents.json";
+import iconReact from "../../assets/icons/react.png";
+import iconAngular from "../../assets/icons/angular.png";
+import iconHtml from "../../assets/icons/html-5.png";
+import iconCss from "../../assets/icons/css-3.png";
+import iconJs from "../../assets/icons/js.png";
 
 export const TecnologiesStack = () => {
-  const tecnologias = [
-    {
-      nombre: "Tecnologia Frontend",
-      descripcion: ["Html", "Css", "Javascript", "React JS", "Wordpress"],
-    },
-    {
-      nombre: "Tecnologias Backend",
-      descripcion: ["Java", "C#", "Python", "Asp net core", "Spring Boot"],
-    },
-    {
-      nombre: "Base de Datos",
-      descripcion: [
-        "MongoDB",
-        "Firebase",
-        "Cassandra",
-        "MySQL",
-        "MS Sql Server",
-      ],
-    },
-    {
-      nombre: "Computacion en la nube",
-      descripcion: ["Amazon Web Services", "Azure", "Red Hat"],
-    },
-    {
-      nombre: "Integracion Continua",
-      descripcion: ["Github", "Gitlab", "Heroku", "Clever Cloud", "Firebase"],
-    },
-    {
-      nombre: "Contenedores Virtuales",
-      descripcion: ["Docker","Azure Container Instances"],
-    },
-    // Puedes agregar más tecnologías según sea necesario
-  ];
 
   return (
-    <Container>
+    <Container >
       <h2 className="text-white mt-4">Stack Tecnológico</h2>
-      {tecnologias.map((tecnologia, index) => (
-        <Row key={index} className="mb-3">
-          <Col>
-            <Card>
-              <Card.Body>
-                <Card.Title>{tecnologia.nombre}</Card.Title>
-                <ul>
-                  {tecnologia.descripcion.map((item, i) => (
-                    <li key={i}>{item}</li>
-                  ))}
-                </ul>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      ))}
+
+
+      <Col className="mb-5">
+        <Card>
+          <Card.Header>{contentStack.home.skills.frontend.titulo}</Card.Header>
+          <Card.Body>
+            <Card.Text>
+              <Row >
+                <Col ><img src={iconHtml} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconCss} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconJs} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconReact} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconAngular} alt="" srcset="" height={"auto"} width={50} /></Col>
+              </Row>
+              <br />
+              <ListGroup>
+                {contentStack.home.skills.frontend.stack.map(skill => {
+                  return (
+                    <ListGroup.Item>{skill} </ListGroup.Item>
+                  )
+                })}
+
+              </ListGroup>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+      </Col>
+      <Col className="mb-5">
+        <Card>
+          <Card.Header>{contentStack.home.skills.backend.titulo}</Card.Header>
+          <Card.Body>
+            <Card.Text>
+            <Row >
+                <Col ><img src={iconHtml} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconCss} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconJs} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconReact} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconAngular} alt="" srcset="" height={"auto"} width={50} /></Col>
+              </Row>
+              <br />
+              <ListGroup>
+                {contentStack.home.skills.backend.stack.map(skill => {
+                  return (
+                    <ListGroup.Item>{skill}</ListGroup.Item>
+                  )
+                })}
+
+              </ListGroup>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+      </Col>
+
+
+
+      <Col className="mb-5">
+        <Card>
+          <Card.Header>{contentStack.home.skills.databases.titulo}</Card.Header>
+          <Card.Body>
+            <Card.Text>
+            <Row >
+                <Col ><img src={iconHtml} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconCss} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconJs} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconReact} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconAngular} alt="" srcset="" height={"auto"} width={50} /></Col>
+              </Row>
+              <br />
+              <ListGroup>
+                {contentStack.home.skills.databases.stack.map(skill => {
+                  return (
+                    <ListGroup.Item>{skill}</ListGroup.Item>
+                  )
+                })}
+
+              </ListGroup>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+      </Col>
+      <Col className="mb-5">
+        <Card>
+          <Card.Header>{contentStack.home.skills.automatizacion.titulo}</Card.Header>
+          <Card.Body>
+            <Card.Text>
+            <Row >
+                <Col ><img src={iconHtml} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconCss} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconJs} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconReact} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconAngular} alt="" srcset="" height={"auto"} width={50} /></Col>
+              </Row>
+              <br />
+              <ListGroup>
+                {contentStack.home.skills.automatizacion.stack.map(skill => {
+                  return (
+                    <ListGroup.Item>{skill}</ListGroup.Item>
+                  )
+                })}
+
+              </ListGroup>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+      </Col>
+
+
+      
+        <Col className="mb-5">
+          <Card>
+            <Card.Header>{contentStack.home.skills.cloud.titulo}</Card.Header>
+            <Card.Body>
+              <Card.Text>
+              <Row >
+                <Col ><img src={iconHtml} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconCss} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconJs} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconReact} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconAngular} alt="" srcset="" height={"auto"} width={50} /></Col>
+              </Row>
+                <br />
+                <ListGroup>
+                  {contentStack.home.skills.cloud.stack.map(skill => {
+                    return (
+                      <ListGroup.Item>{skill}</ListGroup.Item>
+                    )
+                  })}
+
+                </ListGroup>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+        </Col>
+        <Col className="mb-5">
+          <Card>
+            <Card.Header>{contentStack.home.skills.virtualization.titulo}</Card.Header>
+            <Card.Body>
+              <Card.Text>
+              <Row >
+                <Col ><img src={iconHtml} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconCss} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconJs} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconReact} alt="" srcset="" height={"auto"} width={50} /></Col>
+                <Col ><img src={iconAngular} alt="" srcset="" height={"auto"} width={50} /></Col>
+              </Row>
+                <br />
+                <ListGroup>
+                  {contentStack.home.skills.virtualization.stack.map(skill => {
+                    return (
+                      <ListGroup.Item>{skill}</ListGroup.Item>
+                    )
+                  })}
+
+                </ListGroup>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+        </Col>
+
+      
+
     </Container>
   );
 };
